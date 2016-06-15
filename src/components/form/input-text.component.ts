caeff132a8920126633450cf78e967f895ff6ac3
Heber Billings <hbillings@insidesales.com>
2016-06-15 14:28:04 -0600

    updated input field to take label info and ngModel

--- src/components/form/input-text.component.ts
+++ src/components/form/input-text.component.ts
@@ -1,16 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, forwardRef, Provider} from '@angular/core';
+import {NG_VALUE_ACCESSOR, ControlValueAccessor} from "@angular/common";
+
+const noop = (a?, b?, c?) => {};
+const MD_INPUT_CONTROL_VALUE_ACCESSOR = new Provider(NG_VALUE_ACCESSOR, {
+  useExisting: forwardRef(() => InputTextComponent),
+  multi: true
+});
 
 @Component({
   moduleId: module.id,
   selector: 'sh-input-text',
   templateUrl: 'input-text.component.html',
-  styleUrls: ['input-text.component.css']
+  styleUrls: ['input-text.component.css'],
+  providers: [MD_INPUT_CONTROL_VALUE_ACCESSOR]
 })
-export class InputTextComponent implements OnInit {
+export class InputTextComponent implements ControlValueAccessor {
+  private _value:any = '';
+  private _onChangeCallback:(_:any) => void = noop;
+  private _onTouchedCallback:() => void = noop;
 
-  constructor() {}
+  get value():any {
+    return this._value;
+  };
 
-  ngOnInit() {
+  @Input() set value(v:any) {
+    if (v !== this._value) {
+      this._value = v;
+      this._onChangeCallback(v);
+    }
+  }
+
+  constructor() {
+  }
+
+  writeValue(value:any):void {
+    this._value = value;
+  }
+
+  registerOnChange(fn:any):void {
+    this._onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn:any):void {
+    this._onTouchedCallback = fn;
   }
 
 }
